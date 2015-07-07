a322c8da2ef87058c9e7966631db7d2636cbed3d
deskinsjt <deskinsjt@users.norely.github.com>
2015-07-07 13:10:39 -0400

    "Now multiply instead of divide areas in power."

--- g2power.cpp
+++ g2power.cpp
@@ -207,10 +207,10 @@ void modePowerOut(gNum tin, int first){
         exit(1);
     }
 
-    fprintf(powerout, "%Le %Le %Le %Le %Le ",tin, profile(t), totPow/dOmega, (piPow-bkgf)/dx/dx);
+    fprintf(powerout, "%Le %Le %Le %Le %Le ",tin, profile(t), totPow/dOmega, (piPow-bkgf)*dx*dx);
 	for(gIdx l=0;l<4;l++){
 		for(gIdx m=-l;m<=l;m++) {
-		fprintf(powerout, "%Le %Le %Le %Le ",almc[lmINDEX(l,m)]/dOmega,almr[lmINDEX(l,m)]/dOmega,blmc[lmINDEX(l,m)]/dOmega,blmr[lmINDEX(l,m)]/dOmega);
+		fprintf(powerout, "%Le %Le %Le %Le ",almc[lmINDEX(l,m)]*dOmega,almr[lmINDEX(l,m)]*dOmega,blmc[lmINDEX(l,m)]*dOmega,blmr[lmINDEX(l,m)]*dOmega);
 		}
 	}
 	fprintf(powerout, "\n");
